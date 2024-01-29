1c04bf40685a38f0eafd27ff21904a3d803ff438
gonza839 <gonza839@U-XY3X5PA72A8U.uspasDevSimpleAD.fnal.gov>
2024-01-29 11:49:00 -0500

    Trying out SPYDER

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # control-room-AP
-Repository for solutions and notes to the USPAS Winter 2024 Control Room Accelerator Physics class.
+Repository for solutions and notes to the USPAS Winter 2024 Control Room Accelerator Physics class. Edited on VI.
---
+++ try_spyder.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 29 10:35:26 2024
+
+@author: gonza839@uspasDevSimpleAD.fnal.gov
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+print('Hello World')
+
+plt.figure()
+plt.plot()
+plt.show()
